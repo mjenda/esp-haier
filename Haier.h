8fcae37da9959225f10b39ff5787d0258d078fc0
Marek Jenda <edyp87@gmail.com>
2021-12-18 22:18:37 +0100

    Add the ability to on/off the device

--- Haier.h
+++ Haier.h
@@ -343,9 +343,9 @@ protected:
   ClimateTraits traits() override {
     auto traits = climate::ClimateTraits();
     traits.set_supported_modes(
-        {climate::CLIMATE_MODE_HEAT_COOL, climate::CLIMATE_MODE_HEAT,
-         climate::CLIMATE_MODE_COOL, climate::CLIMATE_MODE_DRY,
-         climate::CLIMATE_MODE_FAN_ONLY});
+        {climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_HEAT_COOL,
+         climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_COOL,
+         climate::CLIMATE_MODE_DRY, climate::CLIMATE_MODE_FAN_ONLY});
 
     traits.set_supported_fan_modes(
         {climate::CLIMATE_FAN_ON, climate::CLIMATE_FAN_OFF,
@@ -517,17 +517,9 @@ public:
 
       ESP_LOGD("Control", "*call.get_mode() = %d", new_mode);
 
-      // It seems that this message is no needed, we keep it here commented
-
-      // if((new_mode != CLIMATE_MODE_OFF) && (GetPowerStatus() == false)){
-      // if the current mode is off -> we need to power on
-      //	sendData(power_command, sizeof(power_command));
-      //	delay(1000);
-      //}
-
       switch (new_mode) {
       case CLIMATE_MODE_OFF:
-        SetPowerControl(false);
+        SetPowerControl(!GetPowerStatus());
         sendData(control_command, sizeof(control_command));
         break;
 
